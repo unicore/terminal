bd1efd5703484365392acb230f7f266d03c610f2
Dark Sun <dacom.dark.sun@gmail.com>
2023-05-25 15:37:39 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,11 @@ div
 </template>
 
 
-<script setup lang="ts">//
+<script setup lang="ts">
 import { onUnmounted, onMounted, computed, ref } from 'vue';
 import { useBlockchainStore } from '~/stores/blockchain'
 import { useUserStore } from '~/stores/user'
-
+import { Notify } from 'quasar'
 const bcStore = useBlockchainStore()
 const userStore = useUserStore()
 let intervalId = ref(null);
@@ -16,29 +16,35 @@ import config from '~/config'
 
 
 onMounted(async () => {
-  removeLoader()
-  intervalId.value = setInterval(() => {
-    bcStore.loadInfo()
-  }, 500)
+  try{
+    removeLoader()
+    intervalId.value = setInterval(() => {
+      bcStore.loadInfo()
+    }, 500)
 
-  window.tiledeskSettings= 
-    {
-        projectid: config.tiledeskProject,
-        customAttributes: { 
-          "eosname": userStore.username || "не зарегистрирован", 
-        },
-    };
-    
-  (function(d, s, id) { 
-      var w=window; var d=document; var i=function(){i.c(arguments);};
-      i.q=[]; i.c=function(args){i.q.push(args);}; w.Tiledesk=i;                    
-      var js, fjs=d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js=d.createElement(s); 
-      js.id=id; js.async=true; js.src="https://widget.tiledesk.com/v5/launch.js";
-      fjs.parentNode.insertBefore(js, fjs);
-  }(document,'script','tiledesk-jssdk'));
-
+    window.tiledeskSettings= 
+      {
+          projectid: config.tiledeskProject,
+          customAttributes: { 
+            "eosname": userStore.username || "не зарегистрирован", 
+          },
+      };
+      
+    (function(d, s, id) { 
+        var w=window; var d=document; var i=function(){i.c(arguments);};
+        i.q=[]; i.c=function(args){i.q.push(args);}; w.Tiledesk=i;                    
+        var js, fjs=d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js=d.createElement(s); 
+        js.id=id; js.async=true; js.src="https://widget.tiledesk.com/v5/launch.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document,'script','tiledesk-jssdk'));
+  } catch(e){
+    Notify.create({
+      message: e.message,
+      type: 'negative',
+    })
+  }
 })
 
 
@@ -58,6 +64,11 @@ function removeLoader() {
   const loaderContainer = document.querySelector(".loader-container");
   if (loaderContainer) {
     loaderContainer.remove();
+  } else {
+    Notify.create({
+      message: "Возникла ошибка при загрузке сайта :(",
+      type: 'negative',
+    })
   }
 }
 </script>
